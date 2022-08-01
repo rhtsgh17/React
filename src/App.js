c60c209a46cbe9cce00324be2b7fadcf304f473a
tanya ilham <rhtsgh77@gmail.com>
2022-08-01 15:02:29 +0700

    latihan1

--- src/App.js
+++ src/App.js
@@ -24,18 +24,32 @@
 
 // export default App;
 
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <h1>Latihan Export Import</h1>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+// name export bisa lebih dari 1
+// harus pake kurung kurawal
+// namanya harus sama
+
+
+import React from "react";
+import Latihan from "./latihan-1"
 
 function App() {
-  return (
+  return(
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <h1>Latihan-1</h1>
     </React.Fragment>
   );
 }
 
 export default App;
-
-// name export bisa lebih dari 1
-// harus pake kurung kurawal
-// namanya harus sama
---
+++ src/latihan-1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Page () {
+    return(
+        <React.Fragment>
+            <h1>Latihan1</h1>
+        </React.Fragment>
+    )
+}
