1a96b51c312486c9c83f3346504f9cb05f42fc1f
tanya ilham <rhtsgh77@gmail.com>
2022-08-18 11:11:36 +0700

    nama-commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React from 'react';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <h1 className="bg-red-500">Branch Main</h1>
     </React.Fragment>
   );
 }
