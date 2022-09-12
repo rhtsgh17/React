cac6f728cb308ae9148ef27f5c20579e415bfc8a
tanya ilham <rhtsgh77@gmail.com>
2022-09-12 13:05:28 +0700

    tugas-alquran

--- src/page/Quran.jsx
+++ src/page/Quran.jsx
@@ -1,49 +1,103 @@
+// import React from "react";
+// import axios from "axios";
+// export default function Quran () {
+    
+//     const [users, setUsers] = React.useState([]);  //state utk menyimpan data dari user api
+//         const [page, setPage] = React.useState(1);
+//     const getUserHandle = async() => {
+//             try{
+//                 const response = await axios.get(`https://equran.id/api/surat`);
+//                 console.log("response =>" , response.data);
+//                 setUsers(response.data);
+                
+//             }
+//             catch(err) {
+
+//             }
+//         };
+//         console.log('user =>', users);
+//         console.log('page =>', page);
+
+//         React.useEffect(() => {
+//             getUserHandle();
+//         }, [page]);
+//     return (
+//         <div>
+//             <h1>Tabel Quran</h1>
+//           <div>
+//             {users.map((user, index) => {
+//                     return (
+//                         <tr key={index} className="text-left border">
+//                         <td>{index + 1}</td>
+//                         <td>{user.nomor}</td>
+//                         <td>{user.nama}</td>
+//                         <td>{user.nama_latin}</td>
+//                         <td>{user.jumlah_ayat}</td>
+//                         <td>{user.tempat_turun}</td>
+//                         <td>{user.arti}</td>
+                       
+//                         <td>
+//                             <img className="rounded-full h-5 w-5" src={user.avatar} alt={user.avatar} />
+//                         </td>
+//                         <td><button>Detail</button></td>
+//                     </tr>
+//                     );
+//                    })}
+//           </div>
+//         </div>
+//     )
+// }
+
 import React from "react";
 import axios from "axios";
-export default function Quran () {
-    
-    const [users, setUsers] = React.useState([]);  //state utk menyimpan data dari user api
-        const [page, setPage] = React.useState(1);
-    const getUserHandle = async() => {
-            try{
-                const response = await axios.get(`https://equran.id/api/surat`);
-                console.log("response =>" , response.data);
-                setUsers(response.data);
-                
-            }
-            catch(err) {
+import Card from "./card";
 
-            }
-        };
-        console.log('user =>', users);
-        console.log('page =>', page);
+const User = () => {
+  const [users, setUsers] = React.useState([]);
+  const [page, setPage] = React.useState(1);
 
-        React.useEffect(() => {
-            getUserHandle();
-        }, [page]);
-    return (
-        <div>
-            <h1>Tabel Quran</h1>
-          <div>
-            {users.map((user, index) => {
-                    return (
-                        <tr key={index} className="text-left border">
-                        <td>{index + 1}</td>
-                        <td>{user.nomor}</td>
-                        <td>{user.nama}</td>
-                        <td>{user.nama_latin}</td>
-                        <td>{user.jumlah_ayat}</td>
-                        <td>{user.tempat_turun}</td>
-                        <td>{user.arti}</td>
-                       
-                        <td>
-                            <img className="rounded-full h-5 w-5" src={user.avatar} alt={user.avatar} />
-                        </td>
-                        <td><button>Detail</button></td>
-                    </tr>
-                    );
-                   })}
-          </div>
-        </div>
-    )
-}
+  const getUserHandle = async () => {
+    try {
+      const response = await axios.get(`https://equran.id/api/surat`);
+      console.log("Response =>", response.data);
+
+      setUsers(response.data);
+    } catch (err) {}
+  };
+
+  React.useEffect(() => {
+    getUserHandle();
+  }, [page]);
+
+  console.log("Users =>", users);
+  console.log("Page =>", page);
+  return (
+    <section className="bg-gray-900 w-screen h-full px-15 py-15 pb-15">
+
+      <h1 className="text-[50px] text-white font-bold pb-15 text-center">
+        DAFTAR SURAT<span className="text-violet-500">.</span>
+      </h1>
+      <div className="flex justify-center">
+      <section className="grid grid-cols-1 gap-4 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        {users.map((item, index) => {
+          return (
+            <div>
+              <Card
+                namaLatin={item.nama_latin}
+                arti={item.arti}
+                jumlahAyat={item.jumlah_ayat}
+                tempatTurun={item.tempat_turun}
+                nama={item.nama}
+                nomor={item.nomor}
+              />
+            </div>
+          );
+        })}
+      </section>
+      </div>
+        <p className="text-end text-gray-500 pr-[40px] pt-10">@Rohmats.</p>
+    </section>
+  );
+};
+
+export default User;
--- src/page/card.jsx
+++ src/page/card.jsx
@@ -1,17 +1,44 @@
+// import React from "react";
+
+// export default function Card (nomor, nama_Latin, nama, jumlah_Ayat,tempat_Turun,arti) {
+//     return (
+//         <React.Fragment>
+//             <div>
+//                 <div>{nomor}</div>
+//                 <div>{nama_Latin}</div>
+//                 <div>{nama}</div>
+//                 <div>{jumlah_Ayat}</div>
+//                 <div>{tempat_Turun}</div>
+//                 <div>{arti}</div>
+//             </div>
+
+//         </React.Fragment>
+//     )
+// }
+
 import React from "react";
 
-export default function Card (nomor, nama_Latin, nama, jumlah_Ayat,tempat_Turun,arti) {
-    return (
-        <React.Fragment>
-            <div>
-                <div>{nomor}</div>
-                <div>{nama_Latin}</div>
-                <div>{nama}</div>
-                <div>{jumlah_Ayat}</div>
-                <div>{tempat_Turun}</div>
-                <div>{arti}</div>
-            </div>
-
-        </React.Fragment>
-    )
+function Card({ nomor, namaLatin, jumlahAyat, tempatTurun, arti, nama }) {
+  return (
+    <React.Fragment>
+      <div className="bg-white-800 p-3 w-[300px] h-[200px] rounded-2xl text-green grid content-between border-gray-500 border-b-2 hover:border-b-4 hover:border-violet-500">
+        <div className="rounded-full border-violet-600 flex flex-row justify-between">
+          <p>
+            {nomor}. {namaLatin}
+          </p>
+          <p>{arti}</p>
+        </div>
+        <div className="h-[2px] bg-gray-500"></div>
+        <div className="flex flex-row justify-between text-gray-500 capitalize">
+          <p>{tempatTurun}</p>
+          <p>{jumlahAyat} Ayat</p>
+        </div>
+        <div className="flex-col flex w-full">
+          <p className="text-[65px] text-violet-600 self-end">{nama}</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
+
+export default Card;
