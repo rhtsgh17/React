abb9b5888c03977fc85d84fce0eef315937029b7
tanya ilham <rhtsgh77@gmail.com>
2022-09-28 11:14:13 +0700

    remedial2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React from "react";
-import { Route, Routes, Navigate, NavLink } from "react-router-dom";
-import User from "./page/User";
-import Createuser from "./page/CreateUser";
-import Updateuser from "./page/UpdateUser";
-import Userdetail from "./page/Userdetail";
-import Screen from "./page/Screen";
-import Dashboard from "./page/Dashboard";
-import About from "./page/About";
-
-function App() {
-  return (
-    <React.Fragment>
-      <div className="grid grid-cols-7 justify-center">
-        <section className="col-span-1"></section>
-        <div className="flex flex-col p-5 space-y-10  text-red fixed h-screen w-[10%]">
-          <NavLink to={"/Admin/Dashboard"}>Dashboard</NavLink>
-          <NavLink to={"/Admin/Buku"}>Buku</NavLink>
-          <NavLink to={"/Admin/About"}>About</NavLink>
-        </div>
-        <div className="col-span-6 pr-3 py-5">
-          <Routes>
-            <Route path="/" element={<Screen />}>
-              <Route path="/Admin/Dashboard" element={<Dashboard />} />
-              <Route path="/Admin/Buku" element={<User />}></Route>
-              <Route path="/Admin/Buku/Add" element={<Createuser />} />
-              <Route path="/Admin/Buku/:id/update" element={<Updateuser />} />
-              <Route path="/Admin/Buku/:id/view" element={<Userdetail />} />
-              <Route path="/Admin/About" element={<About />} />
-            </Route>
-
-            <Route path="*" element={<Navigate to="/" replace={true} />} />
-          </Routes>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+let result = 65;
+if (result >= 90) {
+    console.log('selamat anda mendafatkan nilai A');
+}else if (result >= 80) {
+    console.log('selamat anda mendafatkan nilai B');
+}else if (result >= 70) {
+    console.log('selamat anda mendafatkan nilai C');
+}else if (result >= 60) {
+    console.log('selamat anda mendafatkan nilai D');
+}else if (result < 60) {
+    console.log('selamat anda mendafatkan nilai E');
 }
-
-export default App
-
